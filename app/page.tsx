403ecb67377ba7ec6f4b65a8ae223f3bc9726631
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2025-05-12 23:08:47 +0300

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+"use client";
+
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function Model() {
+  const { scene } = useGLTF("/models/bender.glb");
+  return <primitive object={scene} />;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const onClick = () => alert("Вже несемо!");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="fle flex col justify-center py-10">
+      <div className="grid grid-cols-2 gap-10 p-10 rounded-[calc(theme(space.10)+theme(radius-3xl))] bg-gray-500 max-w-5xl">
+        <div className="h-full aspect-square bg-background rounded-3xl">
+          <Canvas camera={{ position: [-3, 3, 4], fov: 45 }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[5, 5, 5]} intensity={0.9} />
+            <Model />
+            <OrbitControls enableZoom />
+          </Canvas>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-col gap-2">
+          <div className=" flex justify-between mb-5">
+            <p className="text-amber-300 font-bold text-5xl">49 200 ₴</p>
+
+            <button
+              className="rounded-full cursor-pointer hover:bg-amber-500 transition-colors duration-300 bg-amber-300 p-3 text-black"
+              type="button"
+              onClick={onClick}
+            >
+              Купити
+            </button>
+          </div>
+
+          <p>
+            <b>Опис:</b> Гранітний пам’ятник з чорного та сірого граніту.
+            Квітник може бути як відкритий, так і закритий. Термін виготовлення
+            4-8 тижні. Також надаються послуги з доставки та встановлення
+            пам’ятника.
+          </p>
+          <div>
+            <p>Розміри:</p>
+            <ul>
+              <li>довжина 196 см;</li>
+              <li>ширина 97 см;</li>
+              <li> висота 155 см.</li>
+            </ul>
+          </div>
+          <p>
+            Отримати додаткову інформацію та замовити пам’ятник ви також можете
+            зателефонувавши до нашої майстерні: контакти.
+          </p>
+
+          <p>Ціна вказана без художніх робіт та аксесуарів.</p>
+        </div>
+      </div>
     </div>
   );
 }
